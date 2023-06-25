540cf119f1d392bbb13580dcff661a6ce303f91f
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-25 20:24:54 +0800

    calculator basic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function (x, y) {
+    return x + y;
+}
+
+const subtract = function (x, y) {
+    return x - y;
+}
+
+const multiply = function (x, y) {
+    return x * y;
+}
+
+const divide = function (x, y) {
+    return x / y;
+}
+
+const operate = function (func, x, y) {
+    return func(x, y);
+}
